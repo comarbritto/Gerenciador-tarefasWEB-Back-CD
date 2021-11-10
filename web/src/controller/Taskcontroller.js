071dd22b98aa210327feb3a9dcb4f23d64fd978e
Omar Britto <comarbritto@gmail.com>
2021-11-10 17:14:48 -0300

    rodando

--- web/src/controller/Taskcontroller.js
+++ web/src/controller/Taskcontroller.js
@@ -1,21 +1,40 @@
-const TaskModel = require('../model/TaskModel');
-//A controller faz o trafego, recebendo e enviando respostas
-class TaskController{
+// const TaskModel = require('../model/TaskModel');
+// //A controller faz o trafego, recebendo e enviando respostas
+// class TaskController{
 
-    //método responsével por receber a requisiçãode armazenamento no banco de dados e criar esse armazenamento 
-    async creat(req, res){
-        //recebe a requisição de front
+//     //método responsével por receber a requisiçãode armazenamento no banco de dados e criar esse armazenamento 
+//     async creat(req, res){
+//         //recebe a requisição de front
+//         const task = new TaskModel(req.body);
+//         //salva no banco de dados
+//         await task
+//         .save() 
+//         .then(response => {return res.status(200).jason(response)})
+//         .catch(error => {return res.status(500).jason(error)});
+
+//         //save() => metodo utilizado para salvar os dados que vem do front no banco de dados
+//         //then() => resposta positiva caso o salvamento for bem sucedido
+//         //catch() => tratamento de erro, caso for mal sucedido
+
+//     }
+// }
+//         module.exports = new TaskController();
+const TaskModel = require('../model/TaskModel');
+
+//A controller faz o tráfego, recebendo e enviando respostas
+class TaskController{
+    //método reponsável por receber a requisição de armazenamento no banco de dados e criar esse armazenamento
+    async create(req, res){
+        //recebe a requisição do front
         const task = new TaskModel(req.body);
         //salva no banco de dados
         await task
         .save() 
-        .then(response => {return res.status(200).jason(response)})
-        .catch(error => {return res.status(500).jason(error)});
-
-        //save() => metodo utilizado para salvar os dados que vem do front no banco de dados
-        //then() => resposta positiva caso o salvamento for bem sucedido
-        //catch() => tratamento de erro, caso for mal sucedido
-
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+        // save() => método utilizado para salvar os dados que vem do front no banco de dados
+        //then() => resposta positiva, caso o salvamento for bem sucedido
+        //catch () => tratamento de erro, caso for mal sucedido
     }
 }
-        module.exports = new TaskController();
+module.exports = new TaskController();
--- web/src/index.js
+++ web/src/index.js
@@ -1,22 +1,10 @@
-// const express = require('express');
-
-// const servidor = express();
-
-// servidor.use(express.json());
-
-// const TaskRouters = require('./routes/TaskRoute');
-
-// servidor.use('/task', TaskRouters);
-
-// servidor.listen(3000)
-
-const express = require('express');
+const express = require('express')
 
 const servidor = express();
 
 servidor.use(express.json());
 
-const TaskRouters = require('./routes/TaskRoute');
+const TaskRouters = require('./routes/TaskRoute')
 
 servidor.use('/task', TaskRouters);
 
