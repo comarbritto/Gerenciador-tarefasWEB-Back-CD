885ad8bae6d1154566bf0b2f08f5c141775f9d35
Omar Britto <comarbritto@gmail.com>
2021-11-20 10:57:02 -0300

    Criando método update

--- web/src/controller/Taskcontroller.js
+++ web/src/controller/Taskcontroller.js
@@ -1,24 +1,3 @@
-// const TaskModel = require('../model/TaskModel');
-// //A controller faz o trafego, recebendo e enviando respostas
-// class TaskController{
-
-//     //método responsével por receber a requisiçãode armazenamento no banco de dados e criar esse armazenamento 
-//     async creat(req, res){
-//         //recebe a requisição de front
-//         const task = new TaskModel(req.body);
-//         //salva no banco de dados
-//         await task
-//         .save() 
-//         .then(response => {return res.status(200).jason(response)})
-//         .catch(error => {return res.status(500).jason(error)});
-
-//         //save() => metodo utilizado para salvar os dados que vem do front no banco de dados
-//         //then() => resposta positiva caso o salvamento for bem sucedido
-//         //catch() => tratamento de erro, caso for mal sucedido
-
-//     }
-// }
-//         module.exports = new TaskController();
 const TaskModel = require('../model/TaskModel');
 
 //A controller faz o tráfego, recebendo e enviando respostas
@@ -35,6 +14,17 @@ class TaskController{
         // save() => método utilizado para salvar os dados que vem do front no banco de dados
         //then() => resposta positiva, caso o salvamento for bem sucedido
         //catch () => tratamento de erro, caso for mal sucedido
+    
     }
+
+    //Método para atualizar
+    async update(req, res){
+
+        await TaskModel
+        .findByIdAndUpdate({'_id':req.params.id}, req.body, {new: true})
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+    }
+
 }
 module.exports = new TaskController();
